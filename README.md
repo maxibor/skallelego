28e51f3c97b6633998a3d124eb890ded4aac5097
maxibor <maxime_borry@eva.mpg.de>
2025-03-06 16:41:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# skallelego
+
+**Scikit-allel lego**: a few utility functions to complement [scikit-allel](https://github.com/cggh/scikit-allel)
+
+Write scikit-allel objects to disk as VCF(.gz)
+
+## Installation
+
+```bash
+pip install git+https
+```
+
+## Quick start
+
+```python
+import scikit-allel
+import skallelego
+
+# Read input VCF with scikit-allel
+callset = allel.read_vcf('example.vcf')
+is_snp = f['variants/is_snp'][:] #np.ndarray
+ref = f['variants/REF'][:].astype('U13') #np.ndarray
+alt = f['variants/ALT'][:].astype('U13') #list
+qual = f['variants/QUAL'][:] if "QUAL" in f['variants'] else None  #np.ndarray
+gt = allel.GenotypeArray(f['calldata/GT'][:])
+samples = f['samples'][:].astype('U13')
+pos = f['variants/POS'][:]
+chrom = f['variants/CHROM'][:].astype('U13')
+
+# Do some filtering/variants operations
+# ...
+
+# Write objects to disk
+skallelego.vcf2disk(
+    samples,
+    chrom,
+    pos,
+    ref,
+    alt,
+    qual,
+    gt,
+    "output.vcf.gz"
+)
+```
+
+## Documentation
+
+```python
+>>>help(skallelego.vcf2disk)
+Help on function vcf2disk in module skallelego:
+
+vcf2disk(samples, chrom, pos, ref, alt, qual, gt, output)
+    Write VCF data to disk. 
+    This function currently only supports writing bi-allelic SNVs to VCF files.
+    
+    Args:
+        samples (np.array): List of sample names of shape (S,)
+        chrom (np.array): List of chromosome identifiers of shape (V,)
+        pos (np.array): List of positions of shape (V,)
+        ref (np.array): List of reference alleles of shape(V,)
+        alt (np.array): List of alternate alleles of shape (V,P). P is currently set to 2
+        qual (np.array): List of variant quality scores of shape(V,)
+        gt (scikit-allel genotype array): Genotype matrix of shape(V,S,P)
+        output(str): path to output file. Use .gz extension to write bgzf compressed VCF files
+    
+    Returns:
+        None
+
+>>> help(skallelego.variant_filter)
+Help on function variant_filter in module skallelego:
+
+variant_filter(gt_in, chrom, pos, ref, alt, qual)
+    Filter variants to only keep non singleton bi-allelic variants, free from LD
+    Args:
+        gt_in(allel.model.ndarray.GenotypeArray): scikit-allel genotype array of shape (V,S,P), P being the ploidy
+        chrom(np.array): array of chromosome names of shape (V,)
+        pos(np.array): array of positions of shape (V,)
+        ref(np.array): array of reference alleles of shape (V,)
+        alt(np.array): array of alternate alleles of shape (V, N) (N being the max number of alternate allels)
+        qual(np.array): array of variant qualities of shape (V,)
+    
+    Returns:
+        np.array: filtered chromosome array, of shape (Vf, )
+        np.array: filtered position array, of shape (Vf, )
+        np.array: filtered reference allele array, of shape (Vf, )
+        np.array: filtered alternate allele array, of shape (Vf, 2)
+        np.array: filtered variant quality array of shape (Vf, )
+```
---
+++ skallelego/__init__.py
@@ -0,0 +1,138 @@
+import vcfpy
+
+def ld_prune(gn, size=500, step=200, threshold=.1, n_iter=2):
+    """
+    Prune variant in Linkage Desequilibrium
+    Snippet adapted from http://alimanfoo.github.io/2015/09/28/fast-pca.html
+
+    Args:
+        gn(allel.model.ndarray.GenotypeArray): scikit-allel genotype array of shape (V,S,P), P being the ploidy
+        size(int): Window size (number of variants).
+        step(int): Number of variants to advance to the next window.
+        threshold(float): Maximum value of r**2 to include variants.
+        n_inter(int): number of iterations
+
+    Returns:
+        allel.model.ndarray.GenotypeArray: LD filtered genotype array of shape (Vf, S, P)
+        list[np.array]: List (of lenght n_iter) of np.array containing indices of positions kept
+    """
+
+    kept_pos = []
+    for i in range(n_iter):
+        _ = []
+        loc_unlinked = allel.locate_unlinked(gn, size=size, step=step, threshold=threshold)
+        _.append(list(loc_unlinked))
+        n = np.count_nonzero(loc_unlinked)
+        n_remove = gn.shape[0] - n
+        print('iteration', i+1, 'retaining', n, 'removing', n_remove, 'variants')
+        gn = gn.compress(loc_unlinked, axis=0)
+        kept_pos.append(_[0])
+    return gn, kept_pos
+
+def variant_filter(gt_in, chrom, pos, ref, alt, qual):
+    """
+    Filter variants to only keep non singleton bi-allelic variants, free from LD
+    Args:
+        gt_in(allel.model.ndarray.GenotypeArray): scikit-allel genotype array of shape (V,S,P), P being the ploidy
+        chrom(np.array): array of chromosome names of shape (V,)
+        pos(np.array): array of positions of shape (V,)
+        ref(np.array): array of reference alleles of shape (V,)
+        alt(np.array): array of alternate alleles of shape (V, N) (N being the max number of alternate allels)
+        qual(np.array): array of variant qualities of shape (V,)
+    
+    Returns:
+        np.array: filtered chromosome array, of shape (Vf, )
+        np.array: filtered position array, of shape (Vf, )
+        np.array: filtered reference allele array, of shape (Vf, )
+        np.array: filtered alternate allele array, of shape (Vf, 2)
+        np.array: filtered variant quality array of shape (Vf, )
+        allel.model.ndarray.GenotypeArray: filtered genotype array of shape (Vf, S, 2)
+    """
+    
+    gt = gt_in.copy()
+    ac = gt.count_alleles()
+
+    # Apply filters
+    biallelic_mask = ac.is_biallelic()  # Only bi-allelic variants
+    non_singleton_mask = ~ac.is_singleton(0) & ~ac.is_singleton(1)  # Exclude singletons
+    snp_mask = is_snp  # Only SNPs
+
+    # Combined mask: SNPs, Bi-allelic, and Non-Singleton
+    final_mask = snp_mask & biallelic_mask & non_singleton_mask
+
+    # Filter variants based on the mask
+    filtered_chrom = chrom[final_mask]
+    filtered_pos = pos[final_mask]
+    filtered_ref = ref[final_mask]
+    filtered_alt = alt[final_mask]
+    filtered_qual = qual[final_mask]
+    filtered_gt = gt[final_mask]
+
+    # Perform LD pruning
+    pruned_gt, non_ld_mask = ld_prune(filtered_gt.to_n_alt(), size=50, step=5, threshold=0.01, n_iter=2)
+
+    # Apply LD mask to variants
+    for m in non_ld_mask:
+        filtered_chrom = filtered_chrom[m]
+        filtered_pos = filtered_pos[m]
+        filtered_ref = filtered_ref[m]
+        filtered_alt = filtered_alt[m][:,:2]
+        filtered_qual = filtered_qual[m]
+        filtered_gt = filtered_gt[m]
+
+        return filtered_chrom, filtered_pos, filtered_ref, filtered_alt, filtered_qual, filtered_gt
+
+
+
+def vcf2disk(samples, chrom, pos, ref, alt, qual, gt, output):
+    """
+    Write VCF data to disk. 
+    This function currently only supports writing bi-allelic SNVs to VCF files.
+
+    Args:
+        samples (np.array): List of sample names of shape (S,)
+        chrom (np.array): List of chromosome identifiers of shape (V,)
+        pos (np.array): List of positions of shape (V,)
+        ref (np.array): List of reference alleles of shape(V,)
+        alt (np.array): List of alternate alleles of shape (V,P). P is currently set to 2
+        qual (np.array): List of variant quality scores of shape(V,)
+        gt (scikit-allel genotype array): Genotype matrix of shape(V,S,P)
+        output(str): path to output file. Use .gz extension to write bgzf compressed VCF files
+
+    Returns:
+        None
+    """
+    
+    # Define VCF Header
+    s = vcfpy.header.SamplesInfos(samples)
+    header = vcfpy.Header()
+    header.samples = s
+
+    # Add metadata
+    header.add_line(vcfpy.HeaderLine(key="fileformat", value="VCFv4.2"))
+    header.add_filter_line({'ID': 'PASS', 'Description': 'All filters passed'})
+    header.add_format_line({'ID': 'GT', 'Number': '1', 'Type': 'String', 'Description': 'Genotype'})
+    with vcfpy.Writer.from_path(output, header) as writer:
+        for i, c in enumerate(chrom):
+            alt_alleles = [vcfpy.Substitution("SNV", a) for a in alt[i] if a != ""]
+
+            calls = []
+            for j, s in enumerate(samples):
+                gt_string = "/".join(list(gt[i,j].astype(str)))  
+                calls.append(vcfpy.Call(s, {"GT": gt_string}))
+
+            record = vcfpy.Record(
+                CHROM=str(c), 
+                POS=int(pos[i]),
+                ID=".", 
+                REF=ref[i],
+                ALT=alt_alleles,
+                QUAL=float(qual[i]) if qual is not None else None,
+                FILTER=["PASS"],  
+                INFO={},  
+                FORMAT=["GT"],
+                calls=calls,
+            )
+
+            # Write the record
+            writer.write_record(record)
